1858d20bf4b7ff1d2dbd8d6ba9917f5aae5e5b5f
Luiz Gustavo <102548854+LuizGustavoHeros@users.noreply.github.com>
2023-02-06 19:29:19 +0000

    Atualização da estilização e da responsividade

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function clicou() {  
   const menu = document.querySelector('nav');
+  // document.querySelectorAll('.info').forEach(elemento => {
+  //   elemento.classList.toggle('add-info')
+  // })
   
   menu.classList.toggle('mudar');
 }
