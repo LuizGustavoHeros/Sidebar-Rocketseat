7bf9fdf40605e01099f41fb802c61ebe7ab3c5c4
Luiz Gustavo <102548854+LuizGustavoHeros@users.noreply.github.com>
2023-02-07 19:05:57 +0000

    Perfil adicionado no footer e titulo

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function clicou() {  
   const menu = document.querySelector('nav');
+  const botao = document.querySelector('button')
+  const exit = document.querySelector('.botao-exit')
+  const logo = document.querySelector('.logo')
+  const foto = document.querySelector('.info-exit')
+  const tittle = document.querySelector('.titulo')
   // document.querySelectorAll('.info').forEach(elemento => {
   //   elemento.classList.toggle('add-info')
   // })
   
+  botao.classList.toggle('botao-ativo')
   menu.classList.toggle('mudar');
+  exit.classList.toggle('botao-exit-ativo')
+  logo.classList.toggle('logo-ativo')
+  foto.classList.toggle('info-exit-active')
+  tittle.classList.toggle('titulo-open')
 }
